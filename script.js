f9d6bd3a0e5b260d8f089fe5b9301a50aa6ef51d
medioqre <xirrel1@gmail.com>
2024-02-10 16:27:08 +0200

    added more texts and fixed some code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let buttonTexts = ['huh', 'no???', 'wth!!!', 'do you want me to get mad?', 'rethink your choice smoly baby', 'end'];
+let buttonTexts = ['huh', 'wha', 'no???', 'wth!!!', 'do you want me to get mad?', 'rethink your choice smoly baby','LAST CHANCE LITTLE LADY', 'THATS IT!!!'];
     let currentTextIndex = 0;
     function changeText() {
         const noButton = document.querySelector('.no');
@@ -6,6 +6,10 @@ let buttonTexts = ['huh', 'no???', 'wth!!!', 'do you want me to get mad?', 'reth
         const currentFontSize = parseFloat(getComputedStyle(noButton).fontSize);
     const newFontSize = currentFontSize * 1.4;  // You can adjust the multiplier for the desired text size increase
 
+    if (buttonTexts[currentTextIndex] === 'THATS IT!!!') {
+        window.location.href = './no.html';
+        return; // Stop further execution if redirected
+    }
     noButton.style.fontSize = newFontSize + 'px';
         // Increment the index or reset it to 0 if it reaches the end
         currentTextIndex = (currentTextIndex + 1) % buttonTexts.length;
